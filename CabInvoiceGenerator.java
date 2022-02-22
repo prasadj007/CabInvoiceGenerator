0452055d0bd596b37125d358f7e691c04ce0e35a
Prasad <prasadj007official@gmail.com>
2022-02-22 21:45:34 +0530

    Added CabInvoice UC1

---
+++ CabInvoiceGenerator.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.InvoiceGenerate;
+
+
+public class CabInvoiceGenerator
+{
+	 private static final int COST_PER_MINUTE = 1;
+	 private static final double MINIMUM_COST_PER_KM = 10;
+	 private static final double MINIMUM_FARE = 5.0;
+
+	    
+	    public double calculateFare(double distance, int time) {
+
+	        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_MINUTE;
+
+	        if(totalFare < MINIMUM_FARE)
+	            return MINIMUM_FARE;
+	        return totalFare;
+		
+		
+	}
+}
+
