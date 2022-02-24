dec014781caecec30f216ca0384b29efeb3051b0
Prasad <prasadj007official@gmail.com>
2022-02-24 17:59:31 +0530

    Added CabInvoiceGeneratorTest to UC2

---
+++ CabInvoiceGeneratorTest.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.InvoiceGenerate;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator inVoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+        inVoiceGenerator = new CabInvoiceGenerator();
+    }
+
+   
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 4.0;
+        int time = 10;
+        double totalFare = inVoiceGenerator.calculateFare(distance, time);
+
+        Assert.assertEquals(50.0,totalFare,0.0);
+    }
+
+   
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 2;
+        double totalFare = inVoiceGenerator.calculateFare(distance, time);
+
+        Assert.assertEquals(5.0,totalFare,0.0);
+    }
+    
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalAggregate() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        double totalAggregate = inVoiceGenerator.calculateFare(rides);
+
+        Assert.assertEquals(30.0,totalAggregate,0.0);
+    }
+}
